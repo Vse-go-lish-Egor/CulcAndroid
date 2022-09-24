b3698c5b821f7d681971511ce98bcc101f0d7c4f
Vse-go-lish-Egor <78629163+Vse-go-lish-Egor@users.noreply.github.com>
2022-09-24 21:58:04 +0300

    commit jopy

--- app/src/main/java/com/example/calculator/Calculation.java
+++ app/src/main/java/com/example/calculator/Calculation.java
@@ -4,6 +4,7 @@ import org.mariuszgromada.math.mxparser.*;
 public class Calculation {
     public String calculateOperation(String mathFunction){
         Expression expression = new Expression(mathFunction);
+
         String result = String.valueOf(expression.calculate());
         return (result.equals("NaN")) ?  "Неверное выражение": result;
     }
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,30 +1,44 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView editText;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.editText);
+        System.out.println("Create");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key", editText.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        editText.setText(savedInstanceState.getString("key"));
     }
     public void onClickNum(View view){
         String text = editText.getText().toString();
         Button pressedButton = (Button)view;
-        if(pressedButton.getText().toString().equals(".")){
-            if(text.contains(".")){
-                text = text.replace(".", "");
-            }
+        if(pressedButton.getText().toString().equals(".")) if (text.contains(".")) {
+            text = text.replace(".", "");
         }
         String resultText = text+pressedButton.getText().toString();
         editText.setText(resultText);
